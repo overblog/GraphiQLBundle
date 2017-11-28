7d1fea027f107def07a5f1bf411b17767b8ee61b
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2017-11-28 19:33:38 +0100

    [Configuration] Removed relay version option

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -25,7 +25,6 @@ final class Configuration implements ConfigurationInterface
                         ->scalarNode('graphiql')->defaultValue('0.11')->end()
                         ->scalarNode('react')->defaultValue('15.6')->end()
                         ->scalarNode('fetch')->defaultValue('2.0')->end()
-                        ->enumNode('relay')->values(['modern', 'classic'])->defaultValue('classic')->end()
                     ->end()
                 ->end()
             ->end()
